60dd3834abde2aab3637934398102df54318aa59
lutfiqaraman <lutfiqaraman@gmail.com>
2022-04-26 17:28:46 +0300

    build a simple histogram using D3.js

--- ch2/js/main.js
+++ ch2/js/main.js
@@ -2,6 +2,8 @@ const svg = d3.select('.responsive-svg-container')
     .append('svg')
         .attr('viewBox', '0 0 1200 1600')
         .style('border', "1px solid black")
+        .style('background', "#F8F0E3")
+
 
 svg.append('rect')
     .attr('x', 10)
