64ebef75d2cfc15c282b43fda484f120db23d582
lutfiqaraman <lutfiqaraman@gmail.com>
2022-05-05 23:46:25 +0300

    format data to be used in a simple histogram using D3.js

--- ch3/js/main.js
+++ ch3/js/main.js
@@ -12,7 +12,7 @@ d3
 // Create the SVG to be used to render the data
 const svg = d3.select('.responsive-svg-container')
     .append('svg')
-    .attr('viewBox', '0 0 1200 1600')
+    .attr('viewBox', '0 0 600 700')
     .style('border', "1px solid black")
     .style('background', "#F8F0E3");
 
