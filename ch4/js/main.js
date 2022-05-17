cfdf0262d9d24c98c91a882306701911363a75e4
lutfiqaraman <lutfiqaraman@gmail.com>
2022-05-17 20:46:30 +0300

    create a line chart using d3js

--- ch4/js/main.js
+++ ch4/js/main.js
@@ -1,10 +1,7 @@
 const csvFilePath = "../ch4/data/weeklyTemperature.csv";
 
 d3
-    .csv(csvFilePath, d => {
-        return {
-            date: d.date
-        }
-    }).then(data => {
+    .csv(csvFilePath, d3.autoType)
+    .then(data => {
         console.log(data);
     });
