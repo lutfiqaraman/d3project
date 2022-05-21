2250b497874e40c8243454a2788564e9dfc343f3
lutfiqaraman <lutfiqaraman@gmail.com>
2022-05-21 16:05:48 +0300

    create a line chart using d3js

--- ch4/js/main.js
+++ ch4/js/main.js
@@ -7,6 +7,7 @@ d3
     });
 
 const drawLineChart = (data) => {
+
     const margin = {
         top: 40,
         right: 170,
@@ -17,8 +18,8 @@ const drawLineChart = (data) => {
     const width = 1000;
     const height = 500;
 
-    const innerWidth  = width - margin.right - margin.left;
-    const innerHeight = height - margin.top - margin.bottom;
+    const innerWidth  = width - (margin.right + margin.left);
+    const innerHeight = height - (margin.top + margin.bottom);
 
     const svg = d3.select("#line-chart")
         .append("svg")
@@ -32,22 +33,43 @@ const drawLineChart = (data) => {
     const lastDate  = d3.max(data, d => d.date);
 
     const xScale = d3.scaleTime()
-        .domain([firstDate, lastDate])
+        .domain([d3.timeMonth.floor(firstDate), lastDate])
         .range([0, innerWidth]);
 
     const maxTemp = d3.max(data, d => d.max_temp_F);
+
     const yScale = d3.scaleLinear()
         .domain([0, maxTemp])
         .range([innerHeight, 0]);
 
-    console.log(maxTemp);
+    const bottomAxis = d3.axisBottom(xScale)
+        .tickFormat(d3.timeFormat("%b"));
 
-    const bottomAxis = d3.axisBottom(xScale);
+    d3.selectAll(".axis-x text")
+        .attr("x", d => {
+            const currentMonth = d;
+            const nextMonth = new Date(2021, currentMonth.getMonth() + 1, 1);
+            return (xScale(nextMonth) - xScale(currentMonth)) / 2;
+        })
+        .attr("y", "10px");
+
     innerChart
         .append("g")
         .attr("class", "axis-x")
         .attr("transform", `translate(0, ${innerHeight})`)
         .call(bottomAxis);
 
+    const leftAxis = d3.axisLeft(yScale);
 
+    innerChart
+        .append("g")
+        .attr("class", "axis-y")
+        .call(leftAxis);
+
+    d3.selectAll(".axis-y text")
+        .attr("x", "-5px");
+
+    d3.selectAll(".axis-x text, .axis-y text")
+        .style("font-family", "Roboto, sans-serif")
+        .style("font-size", "14px");
 }
