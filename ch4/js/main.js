7f8f41337c2f22e70fa96c7ee33154501eeb97c7
lutfiqaraman <lutfiqaraman@gmail.com>
2022-05-25 21:58:38 +0300

    create a line chart using d3js

--- ch4/js/main.js
+++ ch4/js/main.js
@@ -103,5 +103,17 @@ const drawLineChart = (data) => {
         .attr("d", lineGenerator(data))
         .attr("fill", "none")
         .attr("stroke", blueSky);
+
+    const areaGenerator = d3.area()
+        .x(d => xScale(d.date))
+        .y0(d => yScale(d.min_temp_F))
+        .y1(d => yScale(d.max_temp_F))
+        .curve(d3.curveCatmullRom);
+
+    innerChart
+        .append("path")
+        .attr("d", areaGenerator(data))
+        .attr("fill", blueSky)
+        .attr("fill-opacity", 0.2);
 }
 
